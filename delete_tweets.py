65e97cd811087eca9e82a8254227b22186ea7237
Justin G. Mitchell <jgmitchell@gmail.com>
2022-08-12 16:17:24 -0600

    Simple changes

--- delete_tweets.py
+++ delete_tweets.py
@@ -9,7 +9,7 @@ client = tweepy.Client(
     consumer_key=config.CONSUMER_KEY,
     consumer_secret=config.CONSUMER_SECRET,
     access_token=config.ACCESS_TOKEN,
-    access_token_secret=config.ACCESS_TOKEN_SECRET
+    access_token_secret=config.ACCESS_TOKEN_SECRET,
 )
 
 # Important user details
